6cb843e62f07032f2876f60a9c728b6f4c8dbb66
Benson Mokogi <b.mokogi@gmail.com>
2023-04-14 00:04:52 +0300

    solved 8 erros, 2 remaining

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+class Polygon{
+    constructor(sides){
+        this.sides = sides        
+    }
+
+    get countSides(){
+        
+        return this.sides.length;
+
+    }
+
+    get perimeter(){
+        return this.sides.reduce((sum, side) => sum + side);
+    }
+}
+
+const polygon = new Polygon([3,4,5,6])
+
+class Triangle extends Polygon{
+    constructor(...args){
+        super(...args)
+        
+    }
+
+    get isValidTriangle(){
+        if (this.countSides === 3){
+            return true;
+        }
+                    
+    }
+}
+
+class Square extends Polygon{
+    constructor(...args){
+        super(...args)
+       
+    }
+
+    get isValidSquare(){
+        if(this.countSides == 4){
+            return true;
+        }            
+       
+    }
+
+    get area(){
+        return this.sides[0]**2;
+    }
+}
